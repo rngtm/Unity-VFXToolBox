53e88371862a5178a93694e55bc4bc5925ca2f8c
rngtm <r_ngtm@yahoo.co.jp>
2021-01-09 00:11:56 +0900

    refactoring : Splitting the mesh generation method from SpiralMeshGenerator.cs into SpiralMeshGeneratorCore.cs

--- Assets/VfxToolBox/Samples/SampleData/Sample003_ProceduralMesh/VFX/Scripts/SpiralMeshGenerator.cs
+++ Assets/VfxToolBox/Samples/SampleData/Sample003_ProceduralMesh/VFX/Scripts/SpiralMeshGenerator.cs
@@ -7,23 +7,21 @@ namespace VfxToolBox.Sample._003
     [ExecuteInEditMode]
     public class SpiralMeshGenerator : MonoBehaviour
     {
-        [SerializeField] private int curveDivsU = 32;
-        [SerializeField] private int curveDivsV = 32;
-        [SerializeField] private float curveWidth = 0.25f;
-        [SerializeField] private float height = 2f;
-        [SerializeField] private float loops = 2f;
-        [SerializeField] private float roll = 0f;
+        [SerializeField] public int curveDivsU = 32;
+        [SerializeField] public int curveDivsV = 32;
+        [SerializeField] public float curveWidth = 0.25f;
+        [SerializeField] public float height = 2f;
+        [SerializeField] public float loops = 2f;
+        [SerializeField] public float roll = 0f;
 
-        [SerializeField] private AnimationCurve radiusCurve = new AnimationCurve(new Keyframe[]
+        [SerializeField] public AnimationCurve radiusCurve = new AnimationCurve(new Keyframe[]
         {
             new Keyframe(0f, 0f),
             new Keyframe(1f, 1f),
         });
 
-        [SerializeField] private Gradient vertexColorU = new Gradient();
-        [SerializeField] private Gradient vertexColorV = new Gradient();
-
-        [SerializeField] private bool reverseNormal = false;
+        [SerializeField] public Gradient vertexColorU = new Gradient();
+        [SerializeField] public Gradient vertexColorV = new Gradient();
         
         [SerializeField, HideInInspector] private Mesh mesh;
         [SerializeField, HideInInspector] private MeshFilter meshFilter;
@@ -60,123 +58,16 @@ namespace VfxToolBox.Sample._003
         {
             needComputeMesh = true;
         }
-
-        /// <summary>
-        /// メッシュの更新
-        /// </summary>
+        
         void ComputeMesh()
         {
-            curveDivsU = Mathf.Max(2, curveDivsU);
-            curveDivsV = Mathf.Max(3, curveDivsV);
-            curveWidth = Mathf.Max(0f, curveWidth);
-            loops = Mathf.Max(0f, loops);
-            
-            // compute points
-            Vector3[] points;
-            ComputeCurvePoints(out points);
-            
-            // compute vertex uvs, position, color
-            int vertexCount = curveDivsU * curveDivsV * 2;
-            var uv = new Vector2[vertexCount];
-            var tangents = new Vector4[vertexCount]; // along Texture-U direction
-            var vertices = new Vector3[vertexCount];
-            var colors = new Color32[vertexCount];
-            var up = new Vector3(0f, 1f, 0f); // up vector
-            float rollRadian = roll * Mathf.Deg2Rad;
-            for (int vi = 0; vi < curveDivsV; vi++)
-            {
-                // compute vectors
-                var forwardVector =
-                    (vi + 1 < curveDivsV) ? (points[vi + 1] - points[vi]) : (points[vi] - points[vi - 1]);
-                forwardVector = forwardVector.normalized;
-                var rightVector = Vector3.Cross(up, forwardVector).normalized;
-                var normalVector = Vector3.Cross(rightVector, forwardVector).normalized;
-
-                float tv = (float) vi / (curveDivsV - 1); // values in [0, 1]
-                for (int ui = 0; ui < curveDivsU; ui++)
-                {
-                    int vertexIndex = ui + vi * curveDivsU;
-
-                    float tu = (float) ui / (curveDivsU - 1); // values in [0, 1]
-
-                    // position
-                    var tangent = rightVector * Mathf.Cos(rollRadian) + normalVector * Mathf.Sin(rollRadian);
-                    vertices[vertexIndex] = points[vi] + curveWidth * tu * tangent;
-                    tangents[vertexIndex] = new Vector4(tangent.x, tangent.y, tangent.z, 1f);
-
-                    // color
-                    var colorU = vertexColorU.Evaluate(tu);
-                    var colorV = vertexColorV.Evaluate(tv);
-                    colors[vertexIndex] = colorU * colorV;
-
-                    // uv
-                    uv[vertexIndex] = new Vector2(tu, tv);
-                }
-            }
-
-            // compute triangles
-            int triangleCount = (curveDivsU - 1) * (curveDivsV - 1) * 6;
-            int[] triangles = new int[triangleCount];
-            int ti = 0;
-            for (int vi = 0; vi < curveDivsV - 1; vi++)
-            {
-                for (int ui = 0; ui < curveDivsU - 1; ui++)
-                {
-                    int pi = ui + vi * curveDivsU;
-
-                    triangles[ti + 0] = pi; 
-                    triangles[ti + 1] = pi + curveDivsU; 
-                    triangles[ti + 2] = pi + 1;
-
-                    triangles[ti + 3] = pi + 1;
-                    triangles[ti + 4] = pi + curveDivsU;
-                    triangles[ti + 5] = pi + curveDivsU + 1;
-                    ti += 6;
-                }
-            }
-
             if (mesh == null)
             {
                 mesh = new Mesh();
             }
             
-            // clear
-            mesh.triangles = null;
-
-            // set
-            mesh.vertices = vertices;
-            mesh.colors32 = colors;
-            mesh.uv = uv;
-            mesh.triangles = triangles;
-            mesh.tangents = tangents;
-            mesh.RecalculateNormals();
-
+            SpiralMeshGeneratorCore.ComputeMesh(this, mesh);
             meshFilter.mesh = mesh;
-        }
-
-        /// <summary>
-        /// カーブに沿ったポイントを作成
-        /// </summary>
-        private void ComputeCurvePoints(out Vector3[] points)
-        {
-            float curveTimeMin = radiusCurve[0].time;
-            float curveTimeMax = radiusCurve[radiusCurve.length - 1].time;
-            points = new Vector3[curveDivsV];
-            for (int i = 0; i < curveDivsV; i++)
-            {
-                float t = (float) i / (curveDivsV - 1);
-                float radian = t * 2f * Mathf.PI * loops;
-                float radius = radiusCurve.Evaluate(Remap(t, 0f, 1f, curveTimeMin, curveTimeMax));
-                float x = Mathf.Cos(radian) * radius;
-                float y = height * t;
-                float z = Mathf.Sin(radian) * radius;
-                points[i] = new Vector3(x, y, z);
-            }
-        }
-
-        private float Remap(float x, float a, float b, float c, float d)
-        {
-            return (x - a) / (b - a) * (d - c) + c;
         }
 
         private void OnDestroy()
---
+++ Assets/VfxToolBox/Samples/SampleData/Sample003_ProceduralMesh/VFX/Scripts/SpiralMeshGeneratorCore.cs
@@ -0,0 +1,122 @@
+﻿namespace VfxToolBox.Sample._003
+{
+    using UnityEngine;
+
+    public static class SpiralMeshGeneratorCore
+    {
+        /// <summary>
+        /// メッシュの更新
+        /// </summary>
+        public static void ComputeMesh(SpiralMeshGenerator generator, Mesh mesh)
+        {
+            var curveDivsU = Mathf.Max(2, generator.curveDivsU);
+            var curveDivsV = Mathf.Max(3, generator.curveDivsV);
+            var loops = Mathf.Max(0f, generator.loops);
+            var curveWidth = Mathf.Max(0f, generator.curveWidth);
+            var roll = generator.roll;
+            var vertexColorU = generator.vertexColorU;
+            var vertexColorV = generator.vertexColorV;
+            
+            // compute points
+            Vector3[] points;
+            ComputeCurvePoints(generator, out points);
+            
+            // compute vertex uvs, position, color
+            int vertexCount = curveDivsU * curveDivsV * 2;
+            var uv = new Vector2[vertexCount];
+            var tangents = new Vector4[vertexCount]; // along Texture-U direction
+            var vertices = new Vector3[vertexCount];
+            var colors = new Color32[vertexCount];
+            var up = new Vector3(0f, 1f, 0f); // up vector
+            float rollRadian = roll * Mathf.Deg2Rad;
+            for (int vi = 0; vi < curveDivsV; vi++)
+            {
+                // compute vectors
+                var forwardVector =
+                    (vi + 1 < curveDivsV) ? (points[vi + 1] - points[vi]) : (points[vi] - points[vi - 1]);
+                forwardVector = forwardVector.normalized;
+                var rightVector = Vector3.Cross(up, forwardVector).normalized;
+                var normalVector = Vector3.Cross(rightVector, forwardVector).normalized;
+
+                float tv = (float) vi / (curveDivsV - 1); // values in [0, 1]
+                for (int ui = 0; ui < curveDivsU; ui++)
+                {
+                    int vertexIndex = ui + vi * curveDivsU;
+
+                    float tu = (float) ui / (curveDivsU - 1); // values in [0, 1]
+
+                    // position
+                    var tangent = rightVector * Mathf.Cos(rollRadian) + normalVector * Mathf.Sin(rollRadian);
+                    vertices[vertexIndex] = points[vi] + curveWidth * tu * tangent;
+                    tangents[vertexIndex] = new Vector4(tangent.x, tangent.y, tangent.z, 1f);
+
+                    // color
+                    var colorU = vertexColorU.Evaluate(tu);
+                    var colorV = vertexColorV.Evaluate(tv);
+                    colors[vertexIndex] = colorU * colorV;
+
+                    // uv
+                    uv[vertexIndex] = new Vector2(tu, tv);
+                }
+            }
+
+            // compute triangles
+            int triangleCount = (curveDivsU - 1) * (curveDivsV - 1) * 6;
+            int[] triangles = new int[triangleCount];
+            int ti = 0;
+            for (int vi = 0; vi < curveDivsV - 1; vi++)
+            {
+                for (int ui = 0; ui < curveDivsU - 1; ui++)
+                {
+                    int pi = ui + vi * curveDivsU;
+
+                    triangles[ti + 0] = pi; 
+                    triangles[ti + 1] = pi + curveDivsU; 
+                    triangles[ti + 2] = pi + 1;
+
+                    triangles[ti + 3] = pi + 1;
+                    triangles[ti + 4] = pi + curveDivsU;
+                    triangles[ti + 5] = pi + curveDivsU + 1;
+                    ti += 6;
+                }
+            }
+
+
+            // clear
+            mesh.triangles = null;
+
+            // set
+            mesh.vertices = vertices;
+            mesh.colors32 = colors;
+            mesh.uv = uv;
+            mesh.triangles = triangles;
+            mesh.tangents = tangents;
+            mesh.RecalculateNormals();
+        }
+
+        /// <summary>
+        /// カーブに沿ったポイントを作成
+        /// </summary>
+        private static void ComputeCurvePoints(SpiralMeshGenerator generator, out Vector3[] points)
+        {
+            float curveTimeMin = generator.radiusCurve[0].time;
+            float curveTimeMax = generator.radiusCurve[generator.radiusCurve.length - 1].time;
+            points = new Vector3[generator.curveDivsV];
+            for (int i = 0; i < generator.curveDivsV; i++)
+            {
+                float t = (float) i / (generator.curveDivsV - 1);
+                float radian = t * 2f * Mathf.PI * generator.loops;
+                float radius = generator.radiusCurve.Evaluate(Remap(t, 0f, 1f, curveTimeMin, curveTimeMax));
+                float x = Mathf.Cos(radian) * radius;
+                float y = generator.height * t;
+                float z = Mathf.Sin(radian) * radius;
+                points[i] = new Vector3(x, y, z);
+            }
+        }
+
+        private static float Remap(float x, float a, float b, float c, float d)
+        {
+            return (x - a) / (b - a) * (d - c) + c;
+        }
+    }
+}
