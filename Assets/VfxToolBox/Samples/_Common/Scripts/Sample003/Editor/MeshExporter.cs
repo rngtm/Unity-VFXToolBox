080b1c5bcd21f75f4aac98bf02ddda4982afc650
rngtm <r_ngtm@yahoo.co.jp>
2021-01-09 17:02:34 +0900

    refactoring : Splitting the mesh export method from SpiralMeshGeneratorInspector.cs into MeshExporter.cs

---
+++ Assets/VfxToolBox/Samples/_Common/Scripts/Sample003/Editor/MeshExporter.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace VfxToolBox.Sample._003
+{
+    using UnityEditor;
+    using UnityEngine;
+    
+    public static class MeshExporter
+    {
+        /// <summary>
+        /// メッシュを保存するダイアログを表示
+        /// </summary>
+        public static void SaveMeshDialog(Mesh mesh, string dialogTitle, string defaultAssetName)
+        {
+            var savePath = EditorUtility.SaveFilePanelInProject(
+                dialogTitle, defaultAssetName, "asset", "Save Mesh");
+
+            if (string.IsNullOrEmpty(savePath)) return;
+
+            // clone mesh
+            mesh = Object.Instantiate(mesh);
+            MeshUtility.Optimize(mesh);
+
+            var oldAsset = AssetDatabase.LoadAssetAtPath(savePath, typeof(Object));
+            if (oldAsset != null)
+            {
+                // copy
+                EditorUtility.CopySerialized(mesh, oldAsset);
+                AssetDatabase.SaveAssets();
+            }
+            else
+            {
+                // create
+                AssetDatabase.CreateAsset(mesh, savePath);
+                AssetDatabase.Refresh();
+            }
+
+            var asset = AssetDatabase.LoadAssetAtPath(savePath, typeof(Mesh));
+            if (asset == null) return;
+
+            EditorGUIUtility.PingObject(asset);
+        }
+    }
+}
--- Assets/VfxToolBox/Samples/_Common/Scripts/Sample003/Editor/SpiralMeshGeneratorInspector.cs
+++ Assets/VfxToolBox/Samples/_Common/Scripts/Sample003/Editor/SpiralMeshGeneratorInspector.cs
@@ -20,34 +20,8 @@
         {
             var meshGenerator = target as SpiralMeshGenerator;
             if (meshGenerator == null || meshGenerator.Mesh == null) return;
-
-            var savePath = EditorUtility.SaveFilePanelInProject(
-                "Save Spiral Mesh", "New Spiral Mesh.asset", "asset", "Save Mesh");
-
-            if (string.IsNullOrEmpty(savePath)) return;
-
-            // clone mesh
-            var mesh = Instantiate(meshGenerator.Mesh);
-            MeshUtility.Optimize(mesh);
-
-            var oldAsset = AssetDatabase.LoadAssetAtPath(savePath, typeof(Object));
-            if (oldAsset != null)
-            {
-                // copy
-                EditorUtility.CopySerialized(mesh, oldAsset);
-                AssetDatabase.SaveAssets();
-            }
-            else
-            {
-                // create
-                AssetDatabase.CreateAsset(mesh, savePath);
-                AssetDatabase.Refresh();
-            }
-
-            var asset = AssetDatabase.LoadAssetAtPath(savePath, typeof(Mesh));
-            if (asset == null) return;
-
-            EditorGUIUtility.PingObject(asset);
+            
+            MeshExporter.SaveMeshDialog(meshGenerator.Mesh, "Save Spiral Mesh", "New Spiral Mesh.asset");
         }
     }
 }
